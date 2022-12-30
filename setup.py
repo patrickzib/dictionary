a54a8dfc49ae80259ae4eb324ded04c6cfe55d4e
Patrick Schäfer <patrick.schaefer@informatik.hu-berlin.de>
2022-12-30 17:02:21 +0100

    updates to config files

--- setup.py
+++ setup.py
@@ -1,35 +1,36 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = ["patrickzib", "Antoine Guillaume"]
+__author__ = ["patrickzib"]
 
 import codecs
 
 import toml
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup, Extension
+from pathlib import Path
 
 pyproject = toml.load("pyproject.toml")
 
-def long_description():
-    with codecs.open("README.md", encoding="utf-8-sig") as f:
-        return f.read()
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
     
 def setup_package():
     """Set up package."""
     setup(
-        # author_email=pyproject["project"]["authors"][0]["email"],
+        author_email=pyproject["project"]["authors"][0]["email"],
         author=pyproject["project"]["authors"][0]["name"],
         classifiers=pyproject["project"]["classifiers"],
         description=pyproject["project"]["description"],
         # download_url=pyproject["project"]["urls"]["download"],
+        install_requires=pyproject["project"]["dependencies"],
         include_package_data=True,
-        # install_requires=pyproject["project"]["dependencies"],
         keywords=pyproject["project"]["keywords"],
         license=pyproject["project"]["license"],
-        long_description=long_description(),
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         name=pyproject["project"]["name"],
         package_data={
-            "convst": [
+            "weasel": [
                 "*.csv",
                 "*.csv.gz",
                 "*.arff",
