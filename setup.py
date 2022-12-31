f1c2c8937a060eaa0883bd64863b4fd704da6e94
Patrick Schäfer <patrick.schaefer@informatik.hu-berlin.de>
2022-12-31 15:09:43 +0100

    updates

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@
 
 __author__ = ["patrickzib"]
 
-import codecs
-
 import toml
-from setuptools import find_packages, setup, Extension
+from setuptools import find_packages, setup
 from pathlib import Path
 
 pyproject = toml.load("pyproject.toml")
@@ -14,44 +12,38 @@ pyproject = toml.load("pyproject.toml")
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
     
-def setup_package():
-    """Set up package."""
-    setup(
-        author_email=pyproject["project"]["authors"][0]["email"],
-        author=pyproject["project"]["authors"][0]["name"],
-        classifiers=pyproject["project"]["classifiers"],
-        description=pyproject["project"]["description"],
-        # download_url=pyproject["project"]["urls"]["download"],
-        install_requires=pyproject["project"]["dependencies"],
-        include_package_data=True,
-        keywords=pyproject["project"]["keywords"],
-        license=pyproject["project"]["license"],
-        long_description=long_description,
-        long_description_content_type='text/markdown',
-        name=pyproject["project"]["name"],
-        package_data={
-            "weasel": [
-                "*.csv",
-                "*.csv.gz",
-                "*.arff",
-                "*.arff.gz",
-                "*.txt",
-                "*.ts",
-                "*.tsv",
-            ]
-        },
-        packages=find_packages(
-            where=".",
-            exclude=["tests", "tests.*"],
-        ),
-        project_urls=pyproject["project"]["urls"],
-        python_requires=pyproject["project"]["requires-python"],
-        setup_requires=pyproject["build-system"]["requires"],
-        url=pyproject["project"]["urls"]["repository"],
-        version=pyproject["project"]["version"],
-        zip_safe=False,
-    )
-
-
-if __name__ == "__main__":
-    setup_package()
+"""Set up package."""
+setup(
+    author_email=pyproject["project"]["authors"][0]["email"],
+    author=pyproject["project"]["authors"][0]["name"],
+    classifiers=pyproject["project"]["classifiers"],
+    description=pyproject["project"]["description"],
+    install_requires=pyproject["project"]["dependencies"],
+    include_package_data=True,
+    keywords=pyproject["project"]["keywords"],
+    license=pyproject["project"]["license"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name=pyproject["project"]["name"],
+    package_data={
+        "weasel": [
+            "*.csv",
+            "*.csv.gz",
+            "*.arff",
+            "*.arff.gz",
+            "*.txt",
+            "*.ts",
+            "*.tsv",
+        ]
+    },
+    packages=find_packages(
+        where=".",
+        exclude=["tests", "tests.*"],
+    ),
+    project_urls=pyproject["project"]["urls"],
+    python_requires=pyproject["project"]["requires-python"],
+    setup_requires=pyproject["build-system"]["requires"],
+    url=pyproject["project"]["urls"]["repository"],
+    version=pyproject["project"]["version"],
+    zip_safe=False,
+)
